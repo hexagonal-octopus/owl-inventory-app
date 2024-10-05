23f113adf62fd3b6c5c33f937f66bf69086e9ea5
Hexagonal Octopus <hexocto@HexOcto-MacBook.local>
2024-10-05 10:37:21 +0700

    Add env local for FS config

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyACvHH9CntutWDkTsI7yZMowtqSGidEbhs",
-    authDomain: "owl-inventory-app.firebaseapp.com",
-    projectId: "owl-inventory-app",
-    storageBucket: "owl-inventory-app.appspot.com",
-    messagingSenderId: "302508911971",
-    appId: "1:302508911971:web:0940e2f612a75aba1a3756",
-    measurementId: "G-M3VES6YFND"
+    apiKey: process.env.FS_API_KEY,
+    authDomain: process.env.FS_AUTH_DOMAIN,
+    projectId: process.env.FS_PROJECT_ID,
+    storageBucket: process.env.FS_STORAGE_BUCKET,
+    messagingSenderId: process.env.FS_MESSAGING_SENDER_ID,
+    appId: process.env.FS_APP_ID,
+    measurementId: process.env.FS_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig);
